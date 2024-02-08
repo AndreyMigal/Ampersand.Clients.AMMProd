2a8ffd63126f81c272b70755ed40e3fac041ae3a
Andrew Grow <122603923+Andrew-Grow@users.noreply.github.com>
2024-02-08 09:01:51 +0200

    Content

--- src/components/OurProducts/OurProducts.jsx
+++ src/components/OurProducts/OurProducts.jsx
@@ -14,56 +14,58 @@ function OurProducts() {
   const { ref: blockRef, inView: isBlockVisible } = useInView();
   const [isBlockShown, setIsBlockShown] = useState(false);
 
-    useEffect(() => {
+  useEffect(() => {
     if (isBlockVisible) {
       setIsBlockShown(true)
     }
   }, [isBlockVisible])
-  
+
   return (
     <div className={s.root}>
-      <h2 className={classNames(s.title, {[s.titleAnimation]: isBlockShown})} ref={blockRef}>{t('products.title')}</h2>
+      <h2 className={classNames(s.title, { [s.titleAnimation]: isBlockShown })} ref={blockRef}>{t('products.title')}</h2>
       <div className={s.cardsContainer}>
-        <div className={classNames(s.card, {[s.firstCardAnimation]: isBlockShown})}>
+        <div className={classNames(s.card, { [s.firstCardAnimation]: isBlockShown })}>
           <img src={studioDescImg} alt='studio foto' className={s.desctopFoto}></img>
         </div>
         <div className={s.mobilePlaseholder}></div>
-        <div className={classNames(s.card, {[s.secondaCardAnimation]: isBlockShown})}>
+        <div className={classNames(s.card, { [s.secondaCardAnimation]: isBlockShown })}>
           <h3 className={s.cardTitle}><span className={s.line}>/</span>{t('products.firstCard.title')}</h3>
           <ul className={s.carlList}>
-            <li className={s.listItem}><StarIcon/>{t('products.firstCard.firstItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.firstCard.secondItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.firstCard.thirdItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.firstCard.fourthItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.firstCard.fifthItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.firstCard.sixthItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.firstCard.seventhItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.firstCard.eightItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.firstCard.ninthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.firstCard.firstItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.firstCard.secondItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.firstCard.thirdItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.firstCard.fourthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.firstCard.fifthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.firstCard.sixthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.firstCard.seventhItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.firstCard.eightItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.firstCard.ninthItem')}</li>
           </ul>
         </div>
-        <div className={classNames(s.card, {[s.thirdCardAnimation]: isBlockShown})}>
+        <div className={classNames(s.card, { [s.thirdCardAnimation]: isBlockShown })}>
           <h3 className={s.cardTitle}><span className={s.line}>/</span>{t('products.secondCard.title')}</h3>
           <ul className={s.carlList}>
-            <li className={s.listItem}><StarIcon/>{t('products.secondCard.firstItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.secondCard.secondItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.secondCard.thirdItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.secondCard.fourthItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.secondCard.fifthItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.secondCard.sixthItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.secondCard.seventhItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.secondCard.eightItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.secondCard.firstItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.secondCard.secondItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.secondCard.thirdItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.secondCard.fourthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.secondCard.fifthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.secondCard.sixthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.secondCard.seventhItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.secondCard.eightItem')}</li>
           </ul>
         </div>
-        <div className={classNames(s.card, {[s.fourCardAnimation]: isBlockShown})}>
+        <div className={classNames(s.card, { [s.fourCardAnimation]: isBlockShown })}>
           <h3 className={s.cardTitle}><span className={s.line}>/</span>{t('products.thirdCard.title')}</h3>
           <ul className={s.carlList}>
-            <li className={s.listItem}><StarIcon/>{t('products.thirdCard.firstItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.thirdCard.secondItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.thirdCard.thirdItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.thirdCard.fourthItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.thirdCard.fifthItem')}</li>
-            <li className={s.listItem}><StarIcon/>{t('products.thirdCard.sixthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.thirdCard.firstItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.thirdCard.secondItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.thirdCard.thirdItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.thirdCard.fourthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.thirdCard.fifthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.thirdCard.sixthItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.thirdCard.seventhItem')}</li>
+            <li className={s.listItem}><StarIcon />{t('products.thirdCard.eightItem')}</li>
           </ul>
         </div>
         <div className={s.mobilePlaseholder}></div>
